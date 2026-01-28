04d87839904d51a82f9d241ac386dcf5bf7b8567
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-28 12:44:03 -0300

    Feat:
    -mudança de id para guid
    - possibilidade de nulo em data dascimento
    criação de construtor
    -criação de funções criar e atualizar

--- codigo-fonte/src/coFinanceControl.Domain/Models/Usuario/Usuario.cs
+++ codigo-fonte/src/coFinanceControl.Domain/Models/Usuario/Usuario.cs
@@ -5,10 +5,34 @@ namespace CoFinanceControl.Domain.Models.Usuario
     //sealed pra evitar herança e manter integridade.
     public sealed class Usuario
     {
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
         public PrimeiroNome Nome { get; private set; }
         public Sobrenome Sobrenome { get; private set; }
-        public DataNascimento DataNascimento { get; private set; }
+        public DataNascimento? DataNascimento { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public DateTime DataAtualizacao { get; private set; }
+
+        //Privado para garantir que o usuario seja criado apenas pelo método criar
+        private Usuario(){}
         
+        public static Usuario Criar(PrimeiroNome nome, Sobrenome sobrenome, DataNascimento? dataNascimento)
+        {
+            return new Usuario
+            {
+                Id = Guid.NewGuid(),
+                Nome = nome,
+                Sobrenome = sobrenome,
+                DataNascimento = dataNascimento,
+                DataCriacao = DateTime.UtcNow
+            };
+        }
+        
+        public void Atualizar(PrimeiroNome nome, Sobrenome sobrenome, DataNascimento? dataNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = dataNascimento;
+            DataAtualizacao = DateTime.UtcNow;
+        }
     }
 }
