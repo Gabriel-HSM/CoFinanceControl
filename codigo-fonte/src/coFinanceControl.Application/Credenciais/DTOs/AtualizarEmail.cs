99c160b0b8ba5601256026d8902f6a21f42aff56
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-28 12:53:09 -0300

    Remoção de itens antigos

--- codigo-fonte/src/coFinanceControl.Application/Credenciais/DTOs/AtualizarEmail.cs
+++
@@ -1,9 +0,0 @@
-namespace CoFinanceControl.Application.Credenciais.DTOs
-{
-    public sealed record AtualizarEmail
-    {
-        public required Guid Id { get; init; }
-        public required string NovoEmail { get; init; }
-        public required string SenhaAtual { get; init; }
-    }
-}
--- codigo-fonte/src/coFinanceControl.Application/Credenciais/DTOs/AtualizarSenha.cs
+++
@@ -1,9 +0,0 @@
-namespace CoFinanceControl.Application.Credenciais.DTOs
-{
-    public sealed record AtualizarSenha
-    {
-        public required Guid Id { get; init; }
-        public required string SenhaAtual { get; init; }
-        public required string NovaSenha { get; init; }
-    }
-}
