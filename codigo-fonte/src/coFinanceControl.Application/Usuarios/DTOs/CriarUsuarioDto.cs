64788610229c1101faf3fc84a52ca86388a75334
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-28 13:27:37 -0300

    criação de DTOS de suario, criarUsuario e atualizarUsuario

---
+++ codigo-fonte/src/coFinanceControl.Application/Usuarios/DTOs/CriarUsuarioDto.cs
@@ -0,0 +1,13 @@
+namespace CoFinanceControl.Application.Usuarios.DTOs
+{
+    public sealed record CriarUsuarioDto
+    {
+        //Não espera id porque id é interno (usuario não passa)
+        public required string Nome { get; init; }
+        public required string Sobrenome { get; init; }
+        public required DateOnly DataNascimento { get; init; }
+
+        //Não espera dataCriação porque é interno (usuario não passa)
+        
+    }
+}
---
+++ codigo-fonte/src/coFinanceControl.Application/Usuarios/DTOs/UsuarioDto.cs
@@ -0,0 +1,18 @@
+namespace CoFinanceControl.Application.Usuarios.DTOs
+{
+    //Resposta do sistema para o usuario
+    
+    //Faz sentido usar record pq se o usuario tiver o mesmo id/ser igual, ele são o mesmo item
+    //Caixas de transporte não precisam de herança
+    //init define as propriedade apenas na criação do objeto (ou seja atibuir itens a ele depois de ciado, não funciona)
+
+    public sealed record UsuarioDto
+    {
+        public required Guid Id { get; init; }
+        public required string Nome { get; init; }
+        public required string Sobrenome { get; init; }
+        public required DateOnly? DataNascimento { get; init; }
+        public required DateTime DataCriacao { get; init; }
+        public required DateTime DataAtualizacao { get; init; }
+    }
+}
