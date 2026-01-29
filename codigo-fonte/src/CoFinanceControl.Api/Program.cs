d0e720577ff4acb899ee6550f80e678574c43293
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-29 10:47:23 -0300

    Finalização de implementação do Swagger no app

--- codigo-fonte/src/CoFinanceControl.Api/Program.cs
+++ codigo-fonte/src/CoFinanceControl.Api/Program.cs
@@ -5,8 +5,13 @@ builder.Services.AddControllers();
 
 //Swagger
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+//Swagger x2
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.Run();
