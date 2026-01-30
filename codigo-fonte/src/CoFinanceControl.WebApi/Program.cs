b35234cce69ca881fee66db8ea0f62c748b8e3ea
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-30 12:12:36 -0300

    Ajuste de ordem no program e conexão com o banco

--- codigo-fonte/src/CoFinanceControl.WebApi/Program.cs
+++ codigo-fonte/src/CoFinanceControl.WebApi/Program.cs
@@ -2,28 +2,26 @@ using CoFinanceControl.Application.Usuarios.Repositories;
 using CoFinanceControl.Application.Usuarios.Services;
 using CoFinanceControl.Infrastructure.Data;
 using CoFinanceControl.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Conexão com banco
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CoFinanceDbContext>(options => options.UseNpgsql(connectionString));
 
 //Registrar App + infra
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>(); 
 
-//Conexão com banco
-builder.Services.AddDbContext<CoFinanceDbContext>(options =>
-{
-
-});
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
