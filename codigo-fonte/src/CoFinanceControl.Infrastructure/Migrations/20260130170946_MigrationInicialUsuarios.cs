47fabbfe90f4463d21dadbc04720933fd1409dc6
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-01-30 14:13:53 -0300

    MigrationInicial

---
+++ codigo-fonte/src/CoFinanceControl.Infrastructure/Migrations/20260130170946_MigrationInicialUsuarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoFinanceControl.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationInicialUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Sobrenome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: true),
+                    DataCriacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
