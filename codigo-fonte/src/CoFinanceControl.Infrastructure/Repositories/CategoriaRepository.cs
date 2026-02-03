48766c4679d1ad82e0de089ceb1149934d2c06c4
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-03 09:55:20 -0300

    Implementação da interface ICategoriaRepository na classe CategoriaRepository

--- codigo-fonte/src/CoFinanceControl.Infrastructure/Repositories/CategoriaRepository.cs
+++ codigo-fonte/src/CoFinanceControl.Infrastructure/Repositories/CategoriaRepository.cs
@@ -1,7 +1,27 @@
+using CoFinanceControl.Application.Categorias.Repositories;
+using CoFinanceControl.Domain.Models.Categoria;
+using CoFinanceControl.Infrastructure.Data;
+
 namespace CoFinanceControl.Infrastructure.Repositories
 {
-    public class CategoriaRepository
+    public class CategoriaRepository : ICategoriaRepository
     {
-        
+        private readonly CoFinanceDbContext _context;
+
+        public CategoriaRepository(CoFinanceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AdicionarAsync (Categoria categoria, CancellationToken ct = default)
+        {
+            await _context.AddAsync(categoria);
+            await _context.SaveChangesAsync(ct);
+        }
+
+        // public async Task<Categoria?> ObterPorIdAsync (int id, CancellationToken ct = default)
+        // {
+        //     return await _context.
+        // }
     }
 }
