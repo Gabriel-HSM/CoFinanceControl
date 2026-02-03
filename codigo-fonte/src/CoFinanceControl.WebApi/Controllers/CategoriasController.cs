ef905a31ca802bd18c85f3392fe43527848bdb4b
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-03 12:24:53 -0300

    Finalização de endpoints de categoria

--- codigo-fonte/src/CoFinanceControl.WebApi/Controllers/CategoriasController.cs
+++ codigo-fonte/src/CoFinanceControl.WebApi/Controllers/CategoriasController.cs
@@ -29,5 +29,55 @@ namespace MyApp.Namespace
             var categoriaUser = await _service.CriarCategoriaUsuarioAsync(dto.UsuarioId.Value, dto, ct);
             return Ok(categoriaUser);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult>  ObterCategoriaId ([FromRoute] int id, CancellationToken ct = default)
+        {
+            var categoria = await _service.ObterPorIdAsync(id, ct);
+
+            if (categoria is null)
+            return NotFound();
+
+            return Ok(categoria);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarCategoria (
+            [FromRoute] int id,
+            [FromBody] AtualizarCategoriaDto dto,
+            CancellationToken ct = default)
+        {
+            var cateAtualizado = await _service.AtualizarAsync(id, dto, ct);
+
+            if (cateAtualizado is null)
+            return NotFound();
+
+            return Ok(cateAtualizado);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarCategoria (
+            [FromRoute] int id,
+            CancellationToken ct = default
+        )
+        {
+            var deletado = await _service.DeletarAsync(id, ct);
+
+            if (!deletado)
+            {
+                return NotFound();
+            }
+            
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodasCategorias (CancellationToken ct = default)
+        {
+            var categorias = await _service.ObterTodosAsync(ct);
+
+            return Ok(categorias);
+        }
     }
 }
