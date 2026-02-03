e7219c0d653a821082d9da5a15c0dfe6188010ad
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-03 10:33:09 -0300

    implementação finalizada de CategoriaRepository

--- codigo-fonte/src/CoFinanceControl.Infrastructure/Repositories/CategoriaRepository.cs
+++ codigo-fonte/src/CoFinanceControl.Infrastructure/Repositories/CategoriaRepository.cs
@@ -1,6 +1,7 @@
 using CoFinanceControl.Application.Categorias.Repositories;
 using CoFinanceControl.Domain.Models.Categoria;
 using CoFinanceControl.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoFinanceControl.Infrastructure.Repositories
 {
@@ -19,9 +20,35 @@ namespace CoFinanceControl.Infrastructure.Repositories
             await _context.SaveChangesAsync(ct);
         }
 
-        // public async Task<Categoria?> ObterPorIdAsync (int id, CancellationToken ct = default)
-        // {
-        //     return await _context.
-        // }
+        public async Task<Categoria?> ObterPorIdAsync (int id, CancellationToken ct = default)
+        {
+            return await _context.Categorias.FirstOrDefaultAsync(c => c.Id == id, ct);
+        }
+
+        public async Task AtualizarAsync (Categoria categoria, CancellationToken ct = default)
+        {
+            _context.Categorias.Update(categoria);
+            await _context.SaveChangesAsync(ct);
+        }
+
+        public async Task<bool> DeletarAsync (int id, CancellationToken ct = default)
+        {
+            var categoria = await ObterPorIdAsync(id);
+
+            if (categoria is null)
+            return false;
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync(ct);
+
+            return true;
+        }
+
+        public async Task<IEnumerable<Categoria>> ObterTodosAsync (CancellationToken ct = default)
+        {
+             return await _context.Categorias
+                .AsNoTracking()
+                .ToListAsync(ct);
+        }
     }
 }
