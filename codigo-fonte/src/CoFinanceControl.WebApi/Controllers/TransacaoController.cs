80170bfc3e0fe39725de33ba8cfbb2ae4adfe09b
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-05 13:03:27 -0300

    Adiciona controlador Transacao com método para criar transações

---
+++ codigo-fonte/src/CoFinanceControl.WebApi/Controllers/TransacaoController.cs
@@ -0,0 +1,25 @@
+using CoFinanceControl.Application.Transacoes.DTOs;
+using CoFinanceControl.Application.Transacoes.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController : ControllerBase
+    {
+        private ITransacaoService _service;
+
+        TransacaoController(ITransacaoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarTransacao([FromBody]CriarTransacaoDto dto, CancellationToken ct = default)
+        {
+            var transacao = await _service.CriarAsync(dto, ct);
+            return Ok(transacao);
+        }
+    }
+}
