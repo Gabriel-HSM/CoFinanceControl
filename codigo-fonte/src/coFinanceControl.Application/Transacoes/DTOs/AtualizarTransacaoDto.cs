30ee04b8f6c31037457c9edfa4bb7469eff2eaad
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-05 10:35:44 -0300

    Implementação de DTOs de transação

---
+++ codigo-fonte/src/coFinanceControl.Application/Transacoes/DTOs/AtualizarTransacaoDto.cs
@@ -0,0 +1,11 @@
+using CoFinanceControl.Application.Rateios.DTOs;
+
+namespace CoFinanceControl.Application.Transacoes.DTOs
+{
+    public sealed record class AtualizarTransacaoDto
+    {
+        public decimal ValorTotal { get; init; }
+        public required string Descricao { get; init; }
+        public required List<CriarRateioDto> Rateios { get; init; }
+    }
+}
--- codigo-fonte/src/coFinanceControl.Application/Transacoes/DTOs/CriarTransacaoDto.cs
+++ codigo-fonte/src/coFinanceControl.Application/Transacoes/DTOs/CriarTransacaoDto.cs
@@ -2,13 +2,11 @@ using CoFinanceControl.Application.Rateios.DTOs;
 
 namespace CoFinanceControl.Application.Transacoes.DTOs
 {
-    public sealed record class TransacaoDto
+    public sealed record class CriarTransacaoDto
     {
-        public int Id { get; init; }
         public Guid UsuarioId { get; init; }
         public decimal ValorTotal { get; init; }
         public required string Descricao { get; init; }
-        public DateTime DataCriacao { get; init; }
-        public required List<RateioDto> Rateios { get; init; }
+        public required List<CriarRateioDto> Rateios { get; init; }
     }
 }
