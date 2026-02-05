a09b57389fe8acd79268ee9a0e75c924241d4313
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-05 13:46:53 -0300

    Adiciona propriedade 'Destino' como obrigatória em CriarRateioDto e RateioDto

--- codigo-fonte/src/coFinanceControl.Application/Rateios/DTOs/CriarRateioDto.cs
+++ codigo-fonte/src/coFinanceControl.Application/Rateios/DTOs/CriarRateioDto.cs
@@ -3,6 +3,7 @@ namespace CoFinanceControl.Application.Rateios.DTOs
     public sealed record class CriarRateioDto
     {
         public int CategoriaId { get; init; }
+        public required string Destino { get; init; }
         public decimal Valor { get; init; }
     }
 }
--- codigo-fonte/src/coFinanceControl.Application/Rateios/DTOs/RateioDto.cs
+++ codigo-fonte/src/coFinanceControl.Application/Rateios/DTOs/RateioDto.cs
@@ -3,6 +3,7 @@ namespace CoFinanceControl.Application.Rateios.DTOs
     public sealed record class RateioDto
     {
         public int CategoriaId { get; init; }
+        public required string Destino { get; init; }
         public decimal Valor { get; init; }
     }
 }
