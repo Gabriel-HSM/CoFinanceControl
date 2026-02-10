994ca660aec1eb2d0ac700a02580afad42d1d928
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-10 10:54:56 -0300

    Agora possui validação de dados ao atualizar usuário

--- codigo-fonte/src/coFinanceControl.Application/Usuarios/DTOs/AtualizarUsuarioDto.cs
+++ codigo-fonte/src/coFinanceControl.Application/Usuarios/DTOs/AtualizarUsuarioDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using CoFinanceControl.Application.Validations;
+
 namespace CoFinanceControl.Application.Usuarios.DTOs
 {
     public sealed record AtualizarUsuarioDto
     {
+        [MaxLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres")]
+        [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres")]
+        [RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ ]+$", ErrorMessage = "Nome não deve conter números ou caracteres especiais")]
         public string? Nome { get; init; }
+        [MaxLength(50, ErrorMessage = "O sobrenome deve ter no máximo 50 caracteres")]
+        [MinLength(3, ErrorMessage = "O sobrenome deve ter no mínimo 3 caracteres")]
+        [RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ ]+$", ErrorMessage = "Sobrenome não deve conter números ou caracteres especiais")]
         public string? Sobrenome { get; init; }
+         [DataNascimentoValidate(ErrorMessage = "A data de nascimento não deve ser uma data futura")]
         public DateOnly? DataNascimento { get; init; }
     }
 }
