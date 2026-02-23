65d97ead2acaf10b6c7b84ce8c859ff070bea415
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-23 11:43:23 -0300

    Criação de pastas e CredencialDto

---
+++ codigo-fonte/src/coFinanceControl.Application/Credencial/DTOs/Credencial.cs
@@ -0,0 +1,12 @@
+namespace CoFinanceControl.Application.Credencial.DTOs
+{
+    public sealed record class Credencial
+    {
+        public int Id { get; init; }
+        public Guid UsuarioId { get; init; }
+        public required string Email { get; init; }
+        public required string Senha { get; init; }
+        public required DateTime DataCriacao { get; init; }
+        public required DateTime DataAtualizacao { get; init; }
+    }
+}
